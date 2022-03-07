d3b56d3f84c599b23f22a2b4ec9cc9dc7baf2677
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-03-07 06:48:30 -0500

    폼 스타일링 &  커밋  복구

--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -6,6 +6,18 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+import {
+  Button,
+  ContainerLogin,
+  ContainerUser,
+  ErrMsg,
+  InputField,
+  NeedMoreHelp,
+  OffScreen,
+  PasswordRequirements,
+  Title,
+} from 'styles/Auth.styled';
+
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
@@ -71,127 +83,149 @@ const RegisterForm = ({ continueRegister }) => {
   };
 
   return (
-    <div>
-      {errMsg && <div>{errMsg}</div>}
-      <h1 style={{ textAlign: 'center' }}>Welcome to DEV Community</h1>
-      <div onSubmit={handleSubmit}>
-        <label htmlFor="username">
-          Username:
-          {!validName || !user ? (
-            <FontAwesomeIcon icon={faTimes} style={{ display: 'none' }} />
+    <>
+      <Link to="/">
+        <Title>SWFB STUDY</Title>
+      </Link>
+      <ContainerLogin>
+        {errMsg ? <ErrMsg>{errMsg}</ErrMsg> : <OffScreen />}
+        <ContainerUser onSubmit={handleSubmit}>
+          <TitleSignUp>Sign Up</TitleSignUp>
+          <ContainerUserInfo>
+            <ContainerInput>
+              <label htmlFor="username">
+                Username:
+                {!validName || !user ? (
+                  <FontAwesomeIcon icon={faTimes} style={{ display: 'none' }} />
+                ) : (
+                  <FontAwesomeIcon icon={faCheck} style={{ color: 'limegreen' }} />
+                )}
+              </label>
+              <InputField
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off" // dont wanna see the previous values suggested.
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+              />
+            </ContainerInput>
+            {userFocus && !user && !validName && (
+              <PasswordRequirements>
+                <FontAwesomeIcon icon={faInfoCircle} />
+                <br />
+                &nbsp; - 4 to 24 characters.
+                <br />
+                &nbsp; - Must begin with a letter.
+                <br />
+                &nbsp; - Letters, numbers, underscores, hyphens allowed.
+              </PasswordRequirements>
+            )}
+            <ContainerInput>
+              <label htmlFor="password">
+                Password:
+                {validPwd ? (
+                  <FontAwesomeIcon icon={faCheck} style={{ color: 'limegreen' }} />
+                ) : (
+                  <FontAwesomeIcon icon={faCheck} style={{ display: 'none' }} />
+                )}
+                {validPwd || !pwd ? (
+                  <FontAwesomeIcon icon={faTimes} style={{ display: 'none' }} />
+                ) : (
+                  <FontAwesomeIcon icon={faTimes} style={{ color: 'red' }} />
+                )}
+              </label>
+              <InputField
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+                onFocus={() => setPwdFocus(true)}
+                onBlur={() => setPwdFocus(false)}
+              />
+            </ContainerInput>
+            {pwdFocus && !validPwd && (
+              <PasswordRequirements>
+                <FontAwesomeIcon icon={faInfoCircle} /> &nbsp;
+                <br /> &nbsp; - 8 to 24 characters.
+                <br /> &nbsp; - Must include uppercase and lowercase letters, a number and a special character.
+                <br /> &nbsp; - Allowed special characters: ! @ # $ %
+              </PasswordRequirements>
+            )}
+            <ContainerInput>
+              <label htmlFor="confirm_pwd">
+                Confirm Password:
+                {validMatch && matchPwd ? (
+                  <FontAwesomeIcon icon={faCheck} style={{ color: 'limegreen' }} />
+                ) : (
+                  <FontAwesomeIcon icon={faCheck} style={{ display: 'none' }} />
+                )}
+                {validMatch || !matchPwd ? (
+                  <FontAwesomeIcon icon={faTimes} style={{ display: 'none' }} />
+                ) : (
+                  <FontAwesomeIcon icon={faTimes} style={{ color: 'red' }} />
+                )}
+              </label>
+              <InputField
+                type="password"
+                id="confirm_pwd"
+                onChange={(e) => setMatchPwd(e.target.value)}
+                value={matchPwd}
+                required
+                onFocus={() => setMatchFocus(true)}
+                onBlur={() => setMatchFocus(false)}
+              />
+              {matchFocus && !validMatch && (
+                <PasswordRequirements>
+                  <FontAwesomeIcon icon={faInfoCircle} /> &nbsp; Must match the first password input field.
+                </PasswordRequirements>
+              )}
+            </ContainerInput>
+          </ContainerUserInfo>
+          {!validName || !validPwd || !validMatch ? (
+            <Button type="button" disabled>
+              Sign Up
+            </Button>
           ) : (
-            <FontAwesomeIcon icon={faCheck} style={{ color: 'limegreen' }} />
+            <Button type="button"> Sign Up</Button>
           )}
-        </label>
-        <input
-          type="text"
-          id="username"
-          ref={userRef}
-          autoComplete="off" // dont wanna see the previous values suggested.
-          onChange={(e) => setUser(e.target.value)}
-          value={user}
-          required
-          onFocus={() => setUserFocus(true)}
-          onBlur={() => setUserFocus(false)}
-        />
-        {userFocus && !user && !validName ? (
-          <div>
-            <FontAwesomeIcon icon={faInfoCircle} />
-            <br />
-            &nbsp; - 4 to 24 characters.
-            <br />
-            &nbsp; - Must begin with a letter.
-            <br />
-            &nbsp; - Letters, numbers, underscores, hyphens allowed.
-          </div>
-        ) : (
-          <span />
-        )}
+        </ContainerUser>
 
-        <label htmlFor="password">
-          Password:
-          {validPwd ? (
-            <FontAwesomeIcon icon={faCheck} style={{ color: 'limegreen' }} />
-          ) : (
-            <FontAwesomeIcon icon={faCheck} style={{ display: 'none' }} />
-          )}
-          {validPwd || !pwd ? (
-            <FontAwesomeIcon icon={faTimes} style={{ display: 'none' }} />
-          ) : (
-            <FontAwesomeIcon icon={faTimes} style={{ color: 'red' }} />
-          )}
-        </label>
-
-        <input
-          type="password"
-          id="password"
-          onChange={(e) => setPwd(e.target.value)}
-          value={pwd}
-          required
-          onFocus={() => setPwdFocus(true)}
-          onBlur={() => setPwdFocus(false)}
-        />
-        {pwdFocus && !validPwd ? (
-          <div>
-            <FontAwesomeIcon icon={faInfoCircle} /> &nbsp;
-            <br /> &nbsp; - 8 to 24 characters.
-            <br /> &nbsp; - Must include uppercase and lowercase letters, a number and a special character.
-            <br /> &nbsp; - Allowed special characters: ! @ # $ %
-          </div>
-        ) : (
-          <span />
-        )}
-
-        <label htmlFor="confirm_pwd">
-          Confirm Password:
-          {validMatch && matchPwd ? (
-            <FontAwesomeIcon icon={faCheck} style={{ color: 'limegreen' }} />
-          ) : (
-            <FontAwesomeIcon icon={faCheck} style={{ display: 'none' }} />
-          )}
-          {validMatch || !matchPwd ? (
-            <FontAwesomeIcon icon={faTimes} style={{ display: 'none' }} />
-          ) : (
-            <FontAwesomeIcon icon={faTimes} style={{ color: 'red' }} />
-          )}
-        </label>
-        <input
-          type="password"
-          id="confirm_pwd"
-          onChange={(e) => setMatchPwd(e.target.value)}
-          value={matchPwd}
-          required
-          onFocus={() => setMatchFocus(true)}
-          onBlur={() => setMatchFocus(false)}
-        />
-        {matchFocus && !validMatch ? (
-          <div>
-            {' '}
-            <FontAwesomeIcon icon={faInfoCircle} /> &nbsp; Must match the first password input field.
-          </div>
-        ) : (
-          <span />
-        )}
-        {!validName || !validPwd || !validMatch ? (
-          <button type="button" disabled>
-            Sign Up
-          </button>
-        ) : (
-          <button type="button"> Sign Up</button>
-        )}
-      </div>
-
-      <StyledLink to="/login">
-        <span>Already have an account?</span> &nbsp;
-        <span style={{ color: '#4646c7' }}>Log in</span>
-      </StyledLink>
-    </div>
+        <NeedMoreHelp>
+          <Link to="/login">
+            <span>Already have an account?</span> &nbsp;
+            <span style={{ color: '#4646c7' }}>Log in</span>
+          </Link>
+        </NeedMoreHelp>
+      </ContainerLogin>
+    </>
   );
 };
 
-const StyledLink = styled(Link)`
-  text-align: center;
-  text-decoration: none;
+const ContainerUserInfo = styled.div`
+  margin-top: 6rem;
+  margin-bottom: 1rem;
+`;
+
+const ContainerInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+const TitleSignUp = styled.h2`
+  color: #000000;
+  font-family: Roboto;
+  font-weight: 400;
+  font-size: 28px;
+  line-height: 36px;
+  position: absolute;
+  top: 210px;
 `;
 
 export default RegisterForm;
